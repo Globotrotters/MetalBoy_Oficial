f60eda8fc9dad56fb92b4dd4ce6bfd77d1684ad2
Globotrotters <josefelipe121@gmail.com>
2018-04-16 22:22:59 -0300

    MetalBoy
    Repositorio Oficial

---
+++ MetalBoy/Assets/Script/menu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	// Tempo para sair a tela de carregamento
+	float tempoDeCarregamento = 3f;
+
+
+	public GameObject telaDeCarregamento;
+	public GameObject telaDeCreditos;
+	public GameObject menuPrincipal;
+
+	//Botoes
+	public Button[] botoes;
+
+	// Metodos Para o Menu Principal
+	public void IniciarJogo()
+	{
+		//SceneManager.LoadScene ();
+	}
+
+	public void Carregarjogo()
+	{
+		//SceneManager.LoadScene ();
+	}
+
+	public void VoltarAoMenu()
+	{
+		telaDeCreditos.SetActive (false);
+		menuPrincipal.SetActive (true);
+
+	}
+
+	public void TelaDeCredito()
+	{
+		telaDeCreditos.SetActive (true);
+	}
+
+	public void SairDoJogo()
+	{
+		Application.Quit();
+	}
+
+	void Start () 
+	{
+		telaDeCarregamento.SetActive (true);
+
+	}
+	
+
+	void Update () 
+	{
+		tempoDeCarregamento -= Time.deltaTime;
+
+	    if (tempoDeCarregamento < 0.01f)
+		{
+			telaDeCarregamento.SetActive (false);
+			tempoDeCarregamento = 0f;
+		}
+	}
+}
